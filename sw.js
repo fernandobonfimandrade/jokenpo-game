1004441324372b8e40660fcd305b435c9cf8e03b
Fernando Bonfim Andrade <fernandbonfimandrade@gmail.com>
2021-09-17 23:15:54 -0300

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v4';
+const staticCacheName = 'site-static-v5';
 const assets = ['jokenpo-game/index.html', 
 'jokenpo-game/img/star.png', 
 'jokenpo-game/img/defeat.png', 
